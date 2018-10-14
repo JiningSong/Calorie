332ccd11abb0ec11e02118cdc5cdae1c7de92031
Jining song <jiningsmail@gmail.com>
2018-10-13 18:16:31 -0700

    Initial Commit

---
+++ Calorie/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Calorie
+//
+//  Created by Jining song on 2018/10/13.
+//  Copyright © 2018 Jining song. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Calorie/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Calorie
+//
+//  Created by Jining song on 2018/10/13.
+//  Copyright © 2018 Jining song. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
